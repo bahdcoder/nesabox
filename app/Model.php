1a27732ebebc946ecf084fec2df4254122bc893d
Kati Frantz <bahdcoder@gmail.com>
2019-07-06 09:16:13 +0100

    add base model

---
+++ app/Model.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model as BaseModel;
+
+class Model extends BaseModel
+{
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * 
+     * Turn of mass assignment protection
+     * 
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = (string) Str::uuid();
+        });
+    }
+}
