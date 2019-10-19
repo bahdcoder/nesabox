0f425e6a7a0cf92696ecc25c93d2acc935ebfcae
Kati Frantz <bahdcoder@gmail.com>
2019-10-19 23:29:15 +0100

    Install laravel nova

---
+++ app/Providers/NovaServiceProvider.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Cards\Help;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Nova\NovaApplicationServiceProvider;
+
+class NovaServiceProvider extends NovaApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+    }
+
+    /**
+     * Register the Nova routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        Nova::routes()
+                ->withAuthenticationRoutes()
+                ->withPasswordResetRoutes()
+                ->register();
+    }
+
+    /**
+     * Configure the Nova authorization services.
+     *
+     * @return void
+     */
+    protected function authorization()
+    {
+        $this->gate();
+
+        Nova::auth(function ($request) {
+            return app()->environment('local') ||
+                   Gate::check('viewNova', [$request->user()]);
+        });
+    }
+
+    /**
+     * Register the Nova gate.
+     *
+     * This gate determines who can access Nova in non-local environments.
+     *
+     * @return void
+     */
+    protected function gate()
+    {
+        Gate::define('viewNova', function ($user) {
+            return in_array($user->email, [
+                'bahdcoder@gmail.com'
+            ]);
+        });
+    }
+
+    /**
+     * Get the cards that should be displayed on the default Nova dashboard.
+     *
+     * @return array
+     */
+    protected function cards()
+    {
+        return [
+            new Help,
+        ];
+    }
+
+    /**
+     * Get the extra dashboards that should be displayed on the Nova dashboard.
+     *
+     * @return array
+     */
+    protected function dashboards()
+    {
+        return [];
+    }
+
+    /**
+     * Get the tools that should be listed in the Nova sidebar.
+     *
+     * @return array
+     */
+    public function tools()
+    {
+        return [];
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
