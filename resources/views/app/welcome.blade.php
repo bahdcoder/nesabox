24ca1ee800424106437aa6e692e3b37974e89970
Frantz Kati <bahdcoder@gmail.com>
2020-05-15 00:04:15 +0100

    build prod

--- resources/views/app/welcome.blade.php
+++ resources/views/app/welcome.blade.php
@@ -40,6 +40,26 @@
     </head>
 
     <body class="font-sans bg-sky-blue">
+        <div class="relative bg-indigo-600">
+            <div class="max-w-screen-xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+                <div class="pr-16 sm:text-center sm:px-16">
+                    <p class="font-medium text-white">
+                        <span class="md:hidden">
+                            Want to become a node.js cloud developer ?
+                        </span>
+                        <span class="hidden md:inline">
+                            Want to become a pro at managing servers and deploying node.js applications ?
+                        </span>
+                        <span class="block sm:ml-2 sm:inline-block">
+                            <a href="https://deployingnodejs.com" target='_blank' class="text-white font-medium underline">
+                                Get the free course & book &rarr;
+                            </a>
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+
         <div class="relative antialiased overflow-hidden bg-gray-800 hero-bg">
             <div class="max-w-screen-lg mx-auto flex justify-between items-center pt-4 md:pt-6 px-3">
                 <div class="flex items-center">
