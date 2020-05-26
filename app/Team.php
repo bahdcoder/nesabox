aa2d0d7cbfec2754db75065f66c5f6e45ddca8fb
bobbylemm <chidozienwoga@gmail.com>
2020-05-26 09:39:44 +0100

    add more dics

--- app/Team.php
+++ app/Team.php
@@ -25,17 +25,20 @@ class Team extends Model
             return true;
         }
 
-        return (bool) $this->invites()->where('status', 'accepted')
+        return (bool) $this->invites()
+            ->where('status', 'accepted')
             ->where('user_id', $user->id)
             ->first();
     }
 
-    public function hasInvite(User $user) {
+    public function hasInvite(User $user)
+    {
         if ((int) $this->user_id === (int) $user->id) {
             return false;
         }
 
-        return (bool) $this->invites()->where('user_id', $user->id)
+        return (bool) $this->invites()
+            ->where('user_id', $user->id)
             ->first();
     }
 }
---
+++ resources/views/app/docs/sites/git-repositories.blade.php
@@ -0,0 +1,8 @@
+<p class="mb-6">Nesabox make it easy to upload source files by connecting to <span class="font-bold">git</span>.</p>
+<p class="mb-6">
+    To connect to your git repository, navigate to your <span class="underline-sha-green-500" target='_blank' href='/account/source-control'>the source control page.</span> Here, you can connect to where the source files for your site lives, either <span class="underline-sha-green-500" target='_blank' href='https://github.com'>GitHub</span> or <span class="underline-sha-green-500" target='_blank' href='https://gitlab.com'>GitLab</span>
+</p>
+
+@include('app.partials.image-link', [
+    'name' => 'connect-src-ctrl'
+])
